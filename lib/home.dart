029378ae609eea8dc6dfb8484e2ee6af4a26ab99
abduallah <abdullah.mansour97@gmail.com>
2020-11-12 19:33:46 +0200

    calculator stateful

--- lib/home.dart
+++ lib/home.dart
@@ -4,10 +4,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class HomeScreen extends StatelessWidget
+class HomeScreen extends StatefulWidget
 {
   @override
-  Widget build(BuildContext context) {
+  _HomeScreenState createState()
+  {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen>
+{
+  // height var to change height
+  var height = 160;
+  var weight = 60;
+  var age = 20;
+  bool isMale;
+  bool isFeMale;
+
+  @override
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -24,7 +41,6 @@ class HomeScreen extends StatelessWidget
               padding: EdgeInsets.all(20.0),
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.white,
                   borderRadius: BorderRadius.circular(
                     10.0,
                   ),
@@ -33,48 +49,68 @@ class HomeScreen extends StatelessWidget
                 child: Row(
                   children: [
                     Expanded(
-                      child: Container(
-                        color: Colors.teal[100],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.add_circle,
-                              size: 100.0,
-                            ),
-                            SizedBox(
-                              height: 15.0,
-                            ),
-                            Text(
-                              'MALE',
-                              style: TextStyle(
-                                fontSize: 20.0,
+                      child: InkWell(
+                        onTap: ()
+                        {
+                          isMale = true;
+                          isFeMale = false;
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          color: isMale != null && isMale ? Colors.teal[100] : Colors.white,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_circle,
+                                size: 100.0,
                               ),
-                            ),
-                          ],
+                              SizedBox(
+                                height: 15.0,
+                              ),
+                              Text(
+                                'MALE',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                     Expanded(
-                      child: Container(
-                        color: Colors.white,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.add_circle,
-                              size: 100.0,
-                            ),
-                            SizedBox(
-                              height: 15.0,
-                            ),
-                            Text(
-                              'FEMALE',
-                              style: TextStyle(
-                                fontSize: 20.0,
+                      child: InkWell(
+                        onTap: ()
+                        {
+                          isMale = false;
+                          isFeMale = true;
+                          setState(() {
+
+                          });
+                        },
+                        child: Container(
+                          color: isFeMale != null && isFeMale ? Colors.teal[100] : Colors.white,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_circle,
+                                size: 100.0,
                               ),
-                            ),
-                          ],
+                              SizedBox(
+                                height: 15.0,
+                              ),
+                              Text(
+                                'FEMALE',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -113,7 +149,7 @@ class HomeScreen extends StatelessWidget
                       crossAxisAlignment: CrossAxisAlignment.baseline,
                       children: [
                         Text(
-                          '160',
+                          '$height',
                           style: TextStyle(
                               fontSize: 30.0, fontWeight: FontWeight.w700),
                         ),
@@ -121,13 +157,16 @@ class HomeScreen extends StatelessWidget
                       ],
                     ),
                     Slider(
-                      value: 160.0,
+                      value: height.toDouble(),
                       min: 120.0,
                       max: 220.0,
                       activeColor: Colors.teal,
                       inactiveColor: Colors.grey[200],
-                      onChanged: (value) {
-                        print(value.round());
+                      onChanged: (value)
+                      {
+                        setState(() {
+                          height = value.round();
+                        });
                       },
                     ),
                   ],
@@ -138,131 +177,169 @@ class HomeScreen extends StatelessWidget
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(20.0),
-              child: Container(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            10.0,
-                          ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'WEIGHT',
-                              style: TextStyle(
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Text(
-                              '160',
-                              style: TextStyle(
-                                  fontSize: 30.0, fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FloatingActionButton(
-                                  onPressed: (){},
-                                  backgroundColor: Colors.teal,
-                                  child: Icon(
-                                    Icons.minimize,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 15.0,
-                                ),
-                                FloatingActionButton(
-                                  onPressed: (){},
-                                  backgroundColor: Colors.teal,
-                                  child: Icon(
-                                    Icons.add,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          10.0,
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 20.0,
-                    ),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            10.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'WEIGHT',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'AGE',
-                              style: TextStyle(
-                                fontSize: 20.0,
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '$weight',
+                            style: TextStyle(
+                                fontSize: 30.0, fontWeight: FontWeight.w700),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: ()
+                                {
+                                  if(weight != 40)
+                                  {
+                                    weight --;
+                                    setState(() {
+
+                                    });
+                                  }
+                                },
+                                backgroundColor: Colors.teal,
+                                child: Icon(
+                                  Icons.minimize,
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Text(
-                              '20',
-                              style: TextStyle(
-                                  fontSize: 30.0, fontWeight: FontWeight.w700),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                FloatingActionButton(
-                                  onPressed: (){},
-                                  backgroundColor: Colors.teal,
-                                  child: Icon(
-                                    Icons.minimize,
-                                  ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              FloatingActionButton(
+                                onPressed: ()
+                                {
+                                  if(weight != 80)
+                                  {
+                                    weight ++;
+                                    setState(() {
+
+                                    });
+                                  }
+                                },
+                                backgroundColor: Colors.teal,
+                                child: Icon(
+                                  Icons.add,
                                 ),
-                                SizedBox(
-                                  width: 15.0,
-                                ),
-                                FloatingActionButton(
-                                  onPressed: (){},
-                                  backgroundColor: Colors.teal,
-                                  child: Icon(
-                                    Icons.add,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'AGE',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Text(
+                            '$age',
+                            style: TextStyle(
+                                fontSize: 30.0, fontWeight: FontWeight.w700),
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: ()
+                                {
+                                  age --;
+                                  setState(() {
+
+                                  });
+                                },
+                                backgroundColor: Colors.teal,
+                                child: Icon(
+                                  Icons.minimize,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 15.0,
+                              ),
+                              FloatingActionButton(
+                                onPressed: ()
+                                {
+                                  age ++;
+                                  setState(() {
+
+                                  });
+                                },
+                                backgroundColor: Colors.teal,
+                                child: Icon(
+                                  Icons.add,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
           Container(
             color: Colors.teal,
             width: double.infinity,
-            height: 40.0,
+            height: 60.0,
             child: FlatButton(
-              onPressed: () {},
+              onPressed: ()
+              {
+                if(isMale != null && isFeMale != null)
+                {
+                  print(isMale);
+                  print(isFeMale);
+                  print(height);
+                  print(weight);
+                  print(age);
+                }
+              },
               child: Text(
                 'CALCULATE',
                 style: TextStyle(
